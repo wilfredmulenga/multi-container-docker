0c2d15dda46fd59ac3fed1e966a4d968a34d9b18
wilfredmulenga <wilfredmulenga@yahoo.com>
2020-01-11 20:24:30 +0200

    refactor: function to get indexes

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -26,7 +26,7 @@ class Fib extends Component {
   }
 
   renderSeenIndexes () {
-    return this.state.seenIndexes.map(number => number).join(', ')
+    return this.state.seenIndexes.map(item => item.number).join(', ')
   }
 
   renderValues () {
